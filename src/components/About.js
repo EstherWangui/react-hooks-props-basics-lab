507a7a02d6c4e6d6d5aaae6d16086fa3e02008de
EstherWangui <esther.nganga@student.moringaschool.com>
2022-10-01 11:37:34 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
 import React from "react";
+//import user from "../data/user";
+import Link from "./Link";
 
-function About() {
+//function About() {
+function About(props) { 
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
+       <h2>About Me</h2>
+      {props.bio && <p>{props.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      { <Link
+        linkedin={props.links.linkedin}
+        github={props.links.github}
+      />} 
     </div>
   );
 }
